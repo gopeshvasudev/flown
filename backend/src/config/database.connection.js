98c423431b82b176325674e04b37d3ca98f5f797
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2024-12-12 11:40:30 +0530

    created a function for connecting to database

---
+++ backend/src/config/database.connection.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.DATABASE_CONNECTION_URI);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log(`Database connection error: ${error.message}`);
+  }
+}
+
+export default connectToDatabase;
