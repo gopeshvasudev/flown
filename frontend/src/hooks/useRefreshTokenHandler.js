f4446a60e8e5ea3e11616db29ec365a33edc5472
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2025-01-04 10:53:49 +0530

    customized hook for better user experience

--- frontend/src/hooks/useRefreshTokenHandler.js
+++ frontend/src/hooks/useRefreshTokenHandler.js
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
-import axiosInstance from "../utils/axiosInstance.js";
 import { toast } from "react-hot-toast";
 import { useDispatch } from "react-redux";
-import { clearToken, setToken } from "../store/reducers/tokenSlice.js";
-import { clearUser } from "../store/reducers/userSlice.js";
 import { useNavigate } from "react-router-dom";
 
-const useRefreshTokenHandler = () => {
+import axiosInstance from "../utils/axiosInstance.js";
+import { clearUser } from "../store/reducers/userSlice.js";
+import { clearToken, setToken } from "../store/reducers/tokenSlice.js";
+
+const useRefreshTokenHandler = (refreshInterval = 8 * 60 * 1000) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -16,10 +17,12 @@ const useRefreshTokenHandler = () => {
 
       if (res.data.success) {
         dispatch(setToken(res.data.accessToken));
+      } else {
+        throw new Error("Failed to Refresh token");
       }
     } catch (error) {
       if (error && error.response && error.response.data) {
-        toast.error(error?.response?.data?.message);
+        toast.error(error?.response?.data?.message || "Please log in again.");
 
         dispatch(clearToken());
         dispatch(clearUser());
@@ -30,7 +33,10 @@ const useRefreshTokenHandler = () => {
 
   useEffect(() => {
     handler();
-  }, []);
+    const interval = setInterval(() => handler(), refreshInterval);
+
+    return () => clearInterval(interval);
+  }, [refreshInterval]);
 };
 
 export default useRefreshTokenHandler;
